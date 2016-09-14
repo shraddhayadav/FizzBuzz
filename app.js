aacf644ea055a15e3a8b672603a482fd3c71784e
shraddhayadav <shraddhayadavt@gmail.com>
2016-09-14 18:18:41 -0400

    First Committ

---
+++ app.js
@@ -0,0 +1,14 @@
+for( var i = 1; i <= 100; i++){
+	if (i % 3 && i % 5 == 0){
+		document.write("FizzBuzz </br>");
+	}
+	else if (i % 3 == 0){
+		document.write("Fizz </br>");
+	}
+	else if (i % 3 == 0){
+		document.write("Buzz </br>");
+	}
+	else {
+		document.write(i + "</br>");
+	}
+}
